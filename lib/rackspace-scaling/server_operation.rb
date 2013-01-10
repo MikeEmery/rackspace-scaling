3bf71dd247fc2cff8f4ba98167b0dcf36485694a
Mike Emery <philodoxx@gmail.com>
2013-01-10 15:29:20 -0500

    checkpoint

--- lib/rackspace-scaling/server_operation.rb
+++ lib/rackspace-scaling/server_operation.rb
@@ -1,7 +1,24 @@
 module Rackspace
   module Scaling
     class ServerOperation
+      def initialize(auth)
+        @auth = auth
+        @server_endpoint = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/servers"
+        @flavor_endponit = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/flavors"
+        @image_endpoint = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/images"
+      end
       
+      def list_images
+        @image_list ||= begin
+          resp = Typhoeus::Request.get(@endpoint, :headers => { 'X-Auth-Token' => @auth.token, 'Accept' => 'application/json'})
+          parsed_response = JSON.parse(resp.body)
+        end
+      end
+      
+      def list_flavors
+      end
+      
+      def 
     end
   end
 end
