9c587b8c6571a6eefb7ca0f2126e9a78d5652516
Mike Emery <philodoxx@gmail.com>
2013-01-10 15:54:56 -0500

    checkpoint

--- lib/rackspace-scaling.rb
+++ lib/rackspace-scaling.rb
@@ -1,9 +1,10 @@
 require 'typhoeus'
 require 'json'
 require 'net/http'
-require "rackspace-scaling/version"
-require "rackspace-scaling/authentication"
-require "rackspace-scaling/load_balancer_operation"
+require 'rackspace-scaling/version'
+require 'rackspace-scaling/authentication'
+require 'rackspace-scaling/load_balancer_operation'
+require 'rackspace-scaling/server_operation'
 
 module Rackspace
   module Scaling
--- lib/rackspace-scaling/server_operation.rb
+++ lib/rackspace-scaling/server_operation.rb
@@ -1,24 +1,32 @@
 module Rackspace
   module Scaling
     class ServerOperation
-      def initialize(auth)
+      def initialize(auth, region = 'DFW')
         @auth = auth
         @server_endpoint = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/servers"
-        @flavor_endponit = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/flavors"
+        @flavor_endpoint = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/flavors"
         @image_endpoint = @auth.endpoints['cloudServersOpenStack']['endpoints'][region]['publicURL']+"/images"
       end
       
       def list_images
         @image_list ||= begin
-          resp = Typhoeus::Request.get(@endpoint, :headers => { 'X-Auth-Token' => @auth.token, 'Accept' => 'application/json'})
+          resp = Typhoeus::Request.get(@image_endpoint, :headers => { 'X-Auth-Token' => @auth.token, 'Accept' => 'application/json'})
           parsed_response = JSON.parse(resp.body)
         end
       end
       
       def list_flavors
+        @flavor_list ||= begin
+          resp = Typhoeus::Request.get(@flavor_endpoint, :headers => { 'X-Auth-Token' => @auth.token, 'Accept' => 'application/json'})
+          parsed_response = JSON.parse(resp.body)
+        end
       end
       
-      def 
+      def create
+      end
+      
+      def list_instances
+      end
     end
   end
 end
